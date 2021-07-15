5dbf60dbf33d3a9df0524b8a694a5d4b574f884c
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-07-15 12:25:38 +0530

    Mobile Form CSS Fix

--- src/components/Form/styles/Form.js
+++ src/components/Form/styles/Form.js
@@ -12,6 +12,11 @@ export const Container = styled.div`
     max-width: 450px;
     padding: 60px 68px 40px;
     margin-bottom: 100px;
+    
+    @media(max-width: 500px){
+        padding: 20px;
+        width: 80%;
+    }
 `;
 
 export const Error = styled.p`
